b29e9c97b75d5a27914705654dd28a2bc5658c42
Keshav sharma <sharmakeshav54126@gmail.com>
2025-03-09 21:14:05 +0000

    translation service app

---
+++ translationService/app/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel 
+from typing import List,Dict
+
+class TranslationRequest(BaseModel):
+    text: str
+    language: List[str]
+
+class TaskResponse(BaseModel):
+    tast_id: int
+
+class TranslationStatus(BaseModel):
+    task_id: int
+    status: str 
+    translation: Dict[str,str]
